966e7e7dd1614091b96d73e773f56cfa582ad333
esmerlinp <esmerlinep@gmail.com>
2018-02-14 10:17:58 -0400

    add files

---
+++ app/src/main/java/com/esmerlinp/udacitycourses/views/Activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.esmerlinp.udacitycourses.views.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.esmerlinp.udacitycourses.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
